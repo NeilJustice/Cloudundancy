6de6f12d89d7a08805405bde142fc253ebe05857
Neil Justice <njjustice@gmail.com>
2020-10-29 15:34:17 -0500

    Add AcceptanceTesting folder

--- Cloudundancy/CloudundancyMain.cpp
+++ Cloudundancy/CloudundancyMain.cpp
@@ -8,8 +8,15 @@
 // backup-files-and-folders --ini-file=C:\Code\dotfiles\HomeWindowsCloudundancy.ini
 // backup-files-and-folders-to-7z-file --ini-file=C:\Code\dotfiles\HomeWindowsCloudundancy_CodeFolderBackups.ini --7z-ini-file=C:\Code\dotfiles\HomeWindowsCloudundancy_CodeFolderBackups_7zFileCopyStep.ini --backup-staging-folder=C:\CodeFolderBackupStaging
 
-// backup-files-and-folders --ini-file=C:\Code\dotfiles\WorkWindowsCloudundancy.ini
-// backup-files-and-folders-to-7z-file --ini-file=C:\Code\dotfiles\WorkWindowsCloudundancyCodeBackup.ini --7z-ini-file=C:\Code\dotfiles\WorkWindowsCloudundancyCodeBackup7zFile.ini --backup-staging-folder=C:\CodeFolderBackupStaging
+// Acceptance Testing
+// backup-files-and-folders --ini-file=C:\Code\Cloudundancy\AcceptanceTesting\IniFiles\CompletelyBlankIniFile.ini
+// backup-files-and-folders --ini-file=C:\Code\Cloudundancy\AcceptanceTesting\IniFiles\AllThreeHeadingsPresentButBlankIniFile.ini
+// C:\Code\Cloudundancy\AcceptanceTesting\IniFiles\DestinationFolderExistsButNoWriteAccess.ini
+// C:\Code\Cloudundancy\AcceptanceTesting\IniFiles\NonExistentDestinationFolder.ini
+// C:\Code\Cloudundancy\AcceptanceTesting\IniFiles\NonExistentSourceFile.ini
+// C:\Code\Cloudundancy\AcceptanceTesting\IniFiles\NonExistentSourceFolder.ini
+// C:\Code\Cloudundancy\AcceptanceTesting\IniFiles\SourceFileExistsButNoReadAccess.ini
+// C:\Code\Cloudundancy\AcceptanceTesting\IniFiles\SourceFolderExistsButNoReadAccess.ini
 
 int main(int argc, char* argv[])
 {
