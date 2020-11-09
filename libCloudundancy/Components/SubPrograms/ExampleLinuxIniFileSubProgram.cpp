2f5621931c7ecca883cc2b8786aad867dbb8487f
Neil Justice <njjustice@gmail.com>
2020-11-08 20:18:17 -0600

    Groundwork for example-linux-ini-file

--- libCloudundancy/Components/SubPrograms/ExampleLinuxIniFileSubProgram.cpp
+++ libCloudundancy/Components/SubPrograms/ExampleLinuxIniFileSubProgram.cpp
@@ -5,9 +5,38 @@ int ExampleLinuxIniFileSubProgram::Run(const CloudundancyArgs&)
 {
    const string exampleLinuxIniFileText =
 R"(
+Example Linux Cloudundancy .ini file:
+
 [DestinationFolders]
+# Cloud-redundant backups to Google's cloud and GitHub's cloud
+~/GoogleDrive/CloudundancyBackups
+~/GitHubRepos/CloudundancyBackups
+
+# Device-redundant backups to ~/USBDriveA and ~/USBDriveB
+~/USBDriveA
+~/USBDriveB
+
 [SourceFilesAndFolders]
+# In the [SourceFilesAndFolders] section, the format of lines is "<SourceFileOrFolderPath> -> <RelativeDestinationFolderPath>"
+
+# Critical files to backup
+~/GoogleDrive/KeePassFile.kdbx -> .
+~/GoogleDrive/PersonalFinancesSpreadsheet.xlsx -> .
+
+# Critical directories to backup
+Placeholder/Jenkins/jobs/ -> Jenkins/jobs
+
 [FileSubpathsToNotCopy]
+# In the [FileSubpathsToNotCopy] section, case-insensitive file path substrings
+# can be listed so as to not back up files matching the listed case-insensitive file path substrings
+
+# Jenkins jobs folder file paths to not backup so as to only backup Jenkins job config.xml files
+/builds/
+/cobertura/
+/lastStable
+/lastSuccessful/
+/atomic
+nextBuildNumber
 )";
    _console->WriteLine(exampleLinuxIniFileText);
    return 0;
