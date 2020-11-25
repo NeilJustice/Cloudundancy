84c85c45ae697bd8decd768f66a1203f8dc0453d
Neil Justice <njjustice@gmail.com>
2020-11-24 19:19:10 -0600

    Refactor VoidTwoArgMemberFunctionCallerTests

--- libCloudundancy/Components/FunctionCallers/Member/VoidTwoArgMemberFunctionCaller.h
+++ libCloudundancy/Components/FunctionCallers/Member/VoidTwoArgMemberFunctionCaller.h
@@ -5,19 +5,19 @@ class VoidTwoArgMemberFunctionCaller
 {
 public:
    virtual void ConstCall(
-      const ClassType* classPointer,
+      const ClassType* constClassPointer,
       void (ClassType::*constMemberFunction)(Arg1Type, Arg2Type) const,
       Arg1Type arg1, Arg2Type arg2) const
    {
-      (classPointer->*constMemberFunction)(arg1, arg2);
+      (constClassPointer->*constMemberFunction)(arg1, arg2);
    }
 
    virtual void NonConstCall(
-      ClassType* classPointer,
+      ClassType* nonConstClassPointer,
       void (ClassType::*nonConstMemberFunction)(Arg1Type, Arg2Type),
       Arg1Type arg1, Arg2Type arg2) const
    {
-      (classPointer->*nonConstMemberFunction)(arg1, arg2);
+      (nonConstClassPointer->*nonConstMemberFunction)(arg1, arg2);
    }
 
    virtual ~VoidTwoArgMemberFunctionCaller() = default;
--- libCloudundancyTests/Components/FunctionCallers/Member/VoidTwoArgMemberFunctionCallerTests.cpp
+++ libCloudundancyTests/Components/FunctionCallers/Member/VoidTwoArgMemberFunctionCallerTests.cpp
@@ -10,53 +10,51 @@ EVIDENCE
 class Class
 {
 public:
-   mutable vector<pair<Arg1Type, Arg2Type>> calls;
+   mutable vector<pair<Arg1Type, Arg2Type>> functionCallArguments;
 
    void ConstMemberFunction(Arg1Type arg1, Arg2Type arg2) const
    {
-      calls.emplace_back(arg1, arg2);
+      functionCallArguments.emplace_back(arg1, arg2);
    }
 
    void NonConstMemberFunction(Arg1Type arg1, Arg2Type arg2)
    {
-      calls.emplace_back(arg1, arg2);
+      functionCallArguments.emplace_back(arg1, arg2);
    }
 };
 
 TEST(ConstCall_CallsConstMemberFunctionOnce)
 {
-   Class classInstance;
+   const Class constClassInstance;
    VoidTwoArgMemberFunctionCaller<Class, Arg1Type, Arg2Type> voidTwoArgMemberFunctionCaller;
-   IS_EMPTY(classInstance.calls);
    //
-   voidTwoArgMemberFunctionCaller.ConstCall(&classInstance, &Class::ConstMemberFunction, Arg1Type{ 1 }, Arg2Type{ 2 });
+   voidTwoArgMemberFunctionCaller.ConstCall(&constClassInstance, &Class::ConstMemberFunction, Arg1Type{ 1 }, Arg2Type{ 2 });
    //
-   vector<pair<Arg1Type, Arg2Type>> expectedCalls = { { Arg1Type{ 1 }, Arg2Type{ 2 } } };
-   VECTORS_ARE_EQUAL(expectedCalls, classInstance.calls);
+   vector<pair<Arg1Type, Arg2Type>> expectedFunctionCallArguments = { { Arg1Type{ 1 }, Arg2Type{ 2 } } };
+   VECTORS_ARE_EQUAL(expectedFunctionCallArguments, constClassInstance.functionCallArguments);
 
 
-   voidTwoArgMemberFunctionCaller.ConstCall(&classInstance, &Class::ConstMemberFunction, Arg1Type{ 3 }, Arg2Type{ 4 });
+   voidTwoArgMemberFunctionCaller.ConstCall(&constClassInstance, &Class::ConstMemberFunction, Arg1Type{ 3 }, Arg2Type{ 4 });
    //
-   expectedCalls.emplace_back(Arg1Type{ 3 }, Arg2Type{ 4 });
-   VECTORS_ARE_EQUAL(expectedCalls, classInstance.calls);
+   expectedFunctionCallArguments.emplace_back(Arg1Type{ 3 }, Arg2Type{ 4 });
+   VECTORS_ARE_EQUAL(expectedFunctionCallArguments, constClassInstance.functionCallArguments);
 }
 
 TEST(NonConstCall_CallsNonConstMemberFunctionOnce)
 {
    Class classInstance;
    VoidTwoArgMemberFunctionCaller<Class, Arg1Type, Arg2Type> voidTwoArgMemberFunctionCaller;
-   IS_EMPTY(classInstance.calls);
    //
    voidTwoArgMemberFunctionCaller.NonConstCall(&classInstance, &Class::NonConstMemberFunction, Arg1Type{ 1 }, Arg2Type{ 2 });
    //
-   vector<pair<Arg1Type, Arg2Type>> expectedCalls = { { Arg1Type{ 1 }, Arg2Type{ 2 } } };
-   VECTORS_ARE_EQUAL(expectedCalls, classInstance.calls);
+   vector<pair<Arg1Type, Arg2Type>> expectedFunctionCallArguments = { { Arg1Type{ 1 }, Arg2Type{ 2 } } };
+   VECTORS_ARE_EQUAL(expectedFunctionCallArguments, classInstance.functionCallArguments);
 
 
    voidTwoArgMemberFunctionCaller.NonConstCall(&classInstance, &Class::NonConstMemberFunction, Arg1Type{ 3 }, Arg2Type{ 4 });
    //
-   expectedCalls.emplace_back(Arg1Type{ 3 }, Arg2Type{ 4 });
-   VECTORS_ARE_EQUAL(expectedCalls, classInstance.calls);
+   expectedFunctionCallArguments.emplace_back(Arg1Type{ 3 }, Arg2Type{ 4 });
+   VECTORS_ARE_EQUAL(expectedFunctionCallArguments, classInstance.functionCallArguments);
 }
 
 RUN_TEMPLATE_TESTS(VoidTwoArgMemberFunctionCallerTests, int, char)
