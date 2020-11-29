131b8fe1065c44af8dea6246b34c09b314e6561d
Neil Justice <njjustice@gmail.com>
2020-11-29 14:29:20 -0600

    Update libCloudundancyTests/CMakeLists.txt

--- libCloudundancyTests/CloudundancyLibTestMain.cpp
+++ libCloudundancyTests/CloudundancyLibTestMain.cpp
@@ -2,9 +2,6 @@
 
 int main(int argc, char* argv[])
 {
-   [[maybe_unused]] const size_t x = 2ull * 1024ull * 1024ull * 1024ull;
-   cout << x << '\n';
-
    const int zenUnitExitCode = ZenUnit::RunTests(argc, argv);
    return zenUnitExitCode;
 }
