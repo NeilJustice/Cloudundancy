7189f4ac7a387f777310753a9bfd0a1b111413e3
Neil Justice <njjustice@gmail.com>
2020-11-29 20:00:04 -0600

    Refactor docoptvalueRandomTests

--- libCloudundancyTests/ValueTypes/ZenUnit/docoptvalueRandomTests.cpp
+++ libCloudundancyTests/ValueTypes/ZenUnit/docoptvalueRandomTests.cpp
@@ -2,10 +2,10 @@
 #include "libCloudundancyTests/ValueTypes/ZenUnit/docoptvalueRandom.h"
 
 TESTS(docoptvalueRandomTests)
-AFACT(RandomDocoptValue_ReturnsdocoptvalueWithAllNonDefaultFields)
+AFACT(ZenUnitRandomDocoptValue_ReturnsdocoptvalueThatIsEitherALongOrAString)
 EVIDENCE
 
-TEST(RandomDocoptValue_ReturnsdocoptvalueWithAllNonDefaultFields)
+TEST(ZenUnitRandomDocoptValue_ReturnsdocoptvalueThatIsEitherALongOrAString)
 {
    const docopt::Value randomDocoptValue = ZenUnit::Random<docopt::Value>();
    IS_TRUE(randomDocoptValue.IsLong() || randomDocoptValue.IsString());
