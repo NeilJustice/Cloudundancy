e7862db81bd1bd05529fe588a4b526e6354f53c2
Neil Justice <njjustice@gmail.com>
2020-11-29 20:44:41 -0600

    Improve UtilsRandomGeneratorTests

--- libCloudundancyTests/Components/Random/UtilsRandomGeneratorTests.cpp
+++ libCloudundancyTests/Components/Random/UtilsRandomGeneratorTests.cpp
@@ -7,19 +7,24 @@ AFACT(RelativeFolderPath_ReturnsFilesystemPathWith0To2FoldersLeadingToARandomFol
 AFACT(RelativeFolderPaths_ReturnsVectorOfRandomFilesystemPathsBetweenSize0And2)
 EVIDENCE
 
+UtilsRandomGenerator _utilsRandomGenerator;
+
 TEST(RelativeFilePath_ReturnsFilesystemPathWith0To2FoldersLeadingToARandomFilenameWithARandomFileExtension)
 {
-
+   fs::path relativeFilePath = _utilsRandomGenerator.RelativeFilePath();
+   IS_NOT_EMPTY(relativeFilePath);
 }
 
 TEST(RelativeFolderPath_ReturnsFilesystemPathWith0To2FoldersLeadingToARandomFolderName)
 {
-
+   fs::path relativeFolderPath = _utilsRandomGenerator.RelativeFolderPath();
+   IS_NOT_EMPTY(relativeFolderPath);
 }
 
 TEST(RelativeFolderPaths_ReturnsVectorOfRandomFilesystemPathsBetweenSize0And2)
 {
-
+   vector<fs::path> relativeFolderPaths = _utilsRandomGenerator.RelativeFolderPaths();
+   IS_TRUE(relativeFolderPaths.size() <= 2);
 }
 
 RUN_TESTS(UtilsRandomGeneratorTests)
