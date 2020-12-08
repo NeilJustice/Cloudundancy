6919ffa38b584173e1b32a0c6bc0b2570bf88c17
Neil Justice <njjustice@gmail.com>
2020-12-08 16:23:15 -0600

    Add JenkinsJobs/Linux/Cloudundancy-Debug-GCC.sh and JenkinsJobs/Linux/Cloudundancy-Release-GCC.sh

---
+++ JenkinsJobs/Linux/Cloudundancy-Release-GCC.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+export CXX=g++
+python3 -u PyUtils/BuildAndInstallCPlusPlusProgram.py \
+   --solution-name=Cloudundancy \
+   --cmake-generator=Ninja \
+   --cmake-architecture=x64 \
+   --cmake-build-type=Release \
+   --tests-project=libCloudundancyTests \
+   --no-install
