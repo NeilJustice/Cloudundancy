abe42cd5bf5ad8feee005fbb148ea7248ad392df
Neil Justice <njjustice@gmail.com>
2021-01-10 12:24:37 -0600

    Update ZenUnit and MetalMock #include paths to #include 'ZenUnitAndMetalMock/ZenUnit.h' and #include 'ZenUnitAndMetalMock/MetalMock.h'

--- libCloudundancyTests/pch.h
+++ libCloudundancyTests/pch.h
@@ -14,8 +14,8 @@
 namespace fs = std::filesystem;
 using namespace std;
 
-#include "ZenUnit/ZenUnit.h"
-#include "ZenUnit/MetalMock.h"
+#include "ZenUnitAndMetalMock/ZenUnit.h"
+#include "ZenUnitAndMetalMock/MetalMock.h"
 
 // libCloudundancy Enums
 #include "libCloudundancy/Enums/Color.h"
