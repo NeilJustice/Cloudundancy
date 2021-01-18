105d94b6fefcc53e0d44066f27e16fca6894533b
Neil Justice <njjustice@gmail.com>
2021-01-18 16:25:42 -0600

    Update command line arguments text

--- libCloudundancy/ValueTypes/CloudundancyArgs.cpp
+++ libCloudundancy/ValueTypes/CloudundancyArgs.cpp
@@ -3,16 +3,16 @@
 
 const string CloudundancyArgs::CommandLineUsage =
 R"(Cloudundancy v0.9.2
-Copies a list of files and folders to a list of destination folders,
-which could be Google Drive and Microsoft OneDrive automatic cloud-uploading folders.
 https://github.com/NeilJustice/Cloudundancy
 
 Usage:
    Cloudundancy copy-files-to-multiple-folders --ini-file=<CloudundancyIniFilePath> [--delete-destination-folders-first]
+
    Cloudundancy 7zip-files-then-copy-the-7zip-file-to-multiple-folders
       --ini-file-to-copy-files-to-7zip-staging-folder=<CloudundancyIniFilePath>
       --7zip-staging-folder=<FolderPath>
       --ini-file-to-copy-7zip-file-from-staging-folder-to-multiple-folders=<CloudundancyIniFilePath>
+
    Cloudundancy example-linux-ini-file
    Cloudundancy example-windows-ini-file
 )";
--- libCloudundancyTests/ValueTypes/CloudundancyArgsTests.cpp
+++ libCloudundancyTests/ValueTypes/CloudundancyArgsTests.cpp
@@ -10,16 +10,16 @@ TEST(CommandLineUsage_IsExpectedString)
 {
    ARE_EQUAL(
 R"(Cloudundancy v0.9.2
-Copies a list of files and folders to a list of destination folders,
-which could be Google Drive and Microsoft OneDrive automatic cloud-uploading folders.
 https://github.com/NeilJustice/Cloudundancy
 
 Usage:
    Cloudundancy copy-files-to-multiple-folders --ini-file=<CloudundancyIniFilePath> [--delete-destination-folders-first]
+
    Cloudundancy 7zip-files-then-copy-the-7zip-file-to-multiple-folders
       --ini-file-to-copy-files-to-7zip-staging-folder=<CloudundancyIniFilePath>
       --7zip-staging-folder=<FolderPath>
       --ini-file-to-copy-7zip-file-from-staging-folder-to-multiple-folders=<CloudundancyIniFilePath>
+
    Cloudundancy example-linux-ini-file
    Cloudundancy example-windows-ini-file
 )", CloudundancyArgs::CommandLineUsage);
