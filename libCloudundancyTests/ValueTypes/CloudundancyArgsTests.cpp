0c7676b1de02f0df07a2783f245d66ef93a1ed90
Neil Justice <njjustice@gmail.com>
2021-01-19 00:57:17 -0600

    Update command line argument spacing

--- libCloudundancyTests/ValueTypes/CloudundancyArgsTests.cpp
+++ libCloudundancyTests/ValueTypes/CloudundancyArgsTests.cpp
@@ -14,12 +14,10 @@ https://github.com/NeilJustice/Cloudundancy
 
 Usage:
    Cloudundancy copy-files-to-multiple-folders --ini-file=<CloudundancyIniFilePath> [--delete-destination-folders-first]
-
    Cloudundancy 7zip-files-then-copy-the-7zip-file-to-multiple-folders
       --ini-file-to-copy-files-to-7zip-staging-folder=<CloudundancyIniFilePath>
       --7zip-staging-folder=<FolderPath>
       --ini-file-to-copy-7zip-file-from-staging-folder-to-multiple-folders=<CloudundancyIniFilePath>
-
    Cloudundancy example-linux-ini-file
    Cloudundancy example-windows-ini-file
 )", CloudundancyArgs::CommandLineUsage);
