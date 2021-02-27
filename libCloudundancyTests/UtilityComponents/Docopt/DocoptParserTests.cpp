a92719390edfbc84ffafb95c4cf5eea348ee3999
Neil Justice <njjustice@gmail.com>
2021-02-27 16:04:58 -0600

    Fix potential integer overflow in libCloudundancyTests/UtilityComponents/Docopt/DocoptParserTests.cpp

--- libCloudundancyTests/UtilityComponents/Docopt/DocoptParserTests.cpp
+++ libCloudundancyTests/UtilityComponents/Docopt/DocoptParserTests.cpp
@@ -100,7 +100,7 @@ TEST(GetRequiredBool_ArgInMap_ReturnsValue)
 TEST(GetProgramModeSpecificRequiredString_ProgramModeValueDoesNotEqualComparisonProgramModeValue_ReturnsEmptyString)
 {
    const int programModeAsInt = ZenUnit::Random<int>();
-   const int fieldIsRequiredIfProgramModeIntEqualsThisValue = programModeAsInt + 1;
+   const int fieldIsRequiredIfProgramModeIntEqualsThisValue = ZenUnit::RandomNotEqualToValue<int>(programModeAsInt);
    //
    const string argValue = _docoptParser.GetProgramModeSpecificRequiredString(
       _docoptArgs, programModeAsInt, fieldIsRequiredIfProgramModeIntEqualsThisValue, _argName);
