8ad8f0da8226a665f187d46281ad713a803fcdcf
Neil Justice <njjustice@gmail.com>
2021-02-27 23:54:26 -0600

    Update args ordering

--- libCloudundancy/ValueTypes/CloudundancyArgs.cpp
+++ libCloudundancy/ValueTypes/CloudundancyArgs.cpp
@@ -6,11 +6,11 @@ R"(Cloudundancy v0.9.2 - Creates cloud-redundant and device-redundant file backu
 https://github.com/NeilJustice/Cloudundancy
 
 Usage:
+   cloudundancy example-linux-ini-file
+   cloudundancy example-windows-ini-file
    cloudundancy copy-files-to-multiple-folders --ini-file=<CloudundancyIniFilePath> [--delete-destination-folders-first]
    cloudundancy 7zip-files-then-copy-the-7zip-file-to-multiple-folders
       --ini-file-to-copy-files-to-7zip-staging-folder=<CloudundancyIniFilePath>
       --7zip-staging-folder=<FolderPath>
       --ini-file-to-copy-7zip-file-from-staging-folder-to-multiple-folders=<CloudundancyIniFilePath>
-   cloudundancy example-linux-ini-file
-   cloudundancy example-windows-ini-file
 )";
