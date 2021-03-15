47d869b63d461631d4cc964edf4ce9dbdd080fc6
Neil Justice <njjustice@gmail.com>
2021-03-15 09:36:05 -0500

    Updates

--- JenkinsJobs/Linux/Cloudundancy-Cppcheck.sh
+++ JenkinsJobs/Linux/Cloudundancy-Cppcheck.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-set -ev
+set -v
 
 cppcheck \
    --enable=all \
+   --cppcheck-build-dir=Cppcheck \
+   --suppressions-list=CppcheckSuppressions.txt \
+   -D__linux__ \
    -DTEST \
+   -DTHEN_RUN_TEMPLATE_TESTS \
    -I /usr/include/c++/v1 \
    -I /usr/local/include/ZenUnitAndMetalMock \
    -I libCloudundancy \
    -I . \
    -j 64 \
+   --output-file=cppcheck_results.txt \
+   --error-exitcode=1 \
    .
+if [ $? = 1 ]; then
+   echo "Cppcheck failed with exit code 1"
+   exit 1
+fi
 
