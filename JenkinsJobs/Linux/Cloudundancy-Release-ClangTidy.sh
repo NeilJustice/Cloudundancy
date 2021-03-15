3439cf3bd038ed3bfd9190f3d4e6b6969f05af89
Neil Justice <njjustice@gmail.com>
2021-03-15 13:11:01 -0500

    Update JenkinsJobs/Linux/Cloudundancy-Release-ClangTidy.sh

--- JenkinsJobs/Linux/Cloudundancy-Release-ClangTidy.sh
+++ JenkinsJobs/Linux/Cloudundancy-Release-ClangTidy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 
-CXX=clang++ cmake -H. -BRelease -GNinja -DCMAKE_BUILD_TYPE=Release -DClangTidyMode=ON
+export CXX=clang++
+cmake -H. -BRelease -GNinja -DCMAKE_BUILD_TYPE=Release -DClangTidyMode=ON
 (cd Release; ninja clang-tidy)
