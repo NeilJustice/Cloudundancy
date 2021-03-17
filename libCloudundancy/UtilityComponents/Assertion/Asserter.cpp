4a1e762f123d2ae5d781ddca82cb483a656ba06e
Neil Justice <njjustice@gmail.com>
2021-03-17 16:24:03 -0500

    Update String::Concat spacing in Asserter.cpp

--- libCloudundancy/UtilityComponents/Assertion/Asserter.cpp
+++ libCloudundancy/UtilityComponents/Assertion/Asserter.cpp
@@ -7,9 +7,7 @@ void Asserter::ThrowIfIntsNotEqual(int expected, int actual, string_view message
    {
       const string exceptionMessage = String::Concat(
          "Asserter::ThrowIfIntsNotEqual(expected, actual, message) failed due to expected != actual.\n",
-         " expected=", expected, '\n',
-         "   actual=", actual, '\n',
-         "  message=\"", message, "\"");
+         " expected=", expected, '\n', "   actual=", actual, '\n', "  message=\"", message, "\"");
       throw runtime_error(exceptionMessage);
    }
 }
@@ -20,9 +18,7 @@ void Asserter::ThrowIfSizeTsNotEqual(size_t expected, size_t actual, string_view
    {
       const string exceptionMessage = String::Concat(
          "Asserter::ThrowIfSizeTsNotEqual(expected, actual, message) failed due to expected != actual.\n",
-         " expected=", expected, '\n',
-         "   actual=", actual, '\n',
-         "  message=\"", message, "\"");
+         " expected=", expected, '\n', "   actual=", actual, '\n', "  message=\"", message, "\"");
       throw runtime_error(exceptionMessage);
    }
 }
