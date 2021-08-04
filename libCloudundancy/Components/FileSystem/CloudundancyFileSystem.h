fc36f218d8074f39bfeb10f2d860b9ba8d6b8fb9
Neil Justice <njjustice@gmail.com>
2021-08-03 22:13:36 -0500

    Refactoring

--- libCloudundancy/Components/FileSystem/CloudundancyFileSystem.h
+++ libCloudundancy/Components/FileSystem/CloudundancyFileSystem.h
@@ -9,9 +9,9 @@ private:
    bool(*_call_fs_exists_as_assignable_function_overload_pointer)(const fs::path&);
    function<bool(const fs::path&)> _call_fs_exists;
 
-   // using fs_file_size_FunctionOverloadType = unsigned long long(*)(const fs::path&);
-   // unsigned long long(*_call_fs_file_size_as_assignable_function_overload_pointer)(const fs::path&);
-   // function<unsigned long long(const fs::path&)> _call_fs_file_size;
+   using fs_file_size_FunctionOverloadType = unsigned long long(*)(const fs::path&);
+   unsigned long long(*_call_fs_file_size_as_assignable_function_overload_pointer)(const fs::path&);
+   function<unsigned long long(const fs::path&)> _call_fs_file_size;
    // Function Callers
    using _forEacher_DeleteContentsOfFolderExceptForFileNameType = Utils::OneExtraArgMemberFunctionForEacher<CloudundancyFileSystem, fs::path, string_view>;
    unique_ptr<const _forEacher_DeleteContentsOfFolderExceptForFileNameType> _forEacher_DeleteContentsOfFolderExceptForFileName;
