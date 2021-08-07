b57c592400ff95ed562cd2bfa0695a3903e244bc
Neil Justice <njjustice@gmail.com>
2021-08-07 14:50:18 -0500

    Add integration tests for class PassthroughFileSystem

--- libCloudundancyTests/UtilityComponents/FileSystem/PassthroughFileSystemTests.cpp
+++ libCloudundancyTests/UtilityComponents/FileSystem/PassthroughFileSystemTests.cpp
@@ -1,13 +1,37 @@
 #include "pch.h"
 #include "libCloudundancy/UtilityComponents/FileSystem/PassthroughFileSystem.h"
+#include <fstream>
 
 TESTS(PassthroughFileSystemTests)
-AFACT(DefaultConstructor_CodeCoverage)
+AFACT(remove_all_DeletesFolder)
+AFACT(file_size_ReturnsSizeOfFileInBytes)
 EVIDENCE
 
-TEST(DefaultConstructor_CodeCoverage)
+PassthroughFileSystem _passthroughFileSystem;
+
+TEST(remove_all_DeletesFolder)
 {
-   PassthroughFileSystem passthroughFileSystem{};
+   const fs::path folderPath = "remove_all_test_folder";
+   fs::create_directory(folderPath);
+   IS_TRUE(fs::exists(folderPath));
+   //
+   _passthroughFileSystem.remove_all(folderPath);
+   //
+   IS_FALSE(fs::exists(folderPath));
+}
+
+TEST(file_size_ReturnsSizeOfFileInBytes)
+{
+   const fs::path filePath = "file_size_test_file.txt";
+   ofstream f(filePath.c_str());
+   IS_TRUE(f.is_open());
+   f << 0 << 0 << 0;
+   f.close();
+   //
+   const unsigned long long fileSize = _passthroughFileSystem.file_size(filePath);
+   //
+   fs::remove(filePath);
+   ARE_EQUAL(3, fileSize);
 }
 
 RUN_TESTS(PassthroughFileSystemTests)
