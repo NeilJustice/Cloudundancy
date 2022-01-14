11d10b7296e16b1476dae07470e65cc144e8b37d
Neil Justice <njjustice@gmail.com>
2022-01-14 13:38:51 -0600

    Add python.exe -u flag to JenkinsJobs/Windows/CloudundancyDevOpsPython-Mypy-Flake8-Pylint-SonarQube.ps1

--- JenkinsJobs/Windows/CloudundancyDevOpsPython-Mypy-Flake8-Pylint-SonarQube.ps1
+++ JenkinsJobs/Windows/CloudundancyDevOpsPython-Mypy-Flake8-Pylint-SonarQube.ps1
@@ -1,7 +1,3 @@
-Set-PSDebug -Trace 1
-
 $env:PYTHONPATH="."
-python.exe CloudundancyDevOpsPython\MypyFlake8PylintThenRunTestsWithCoverage.py --run-tests-with-coverage-python-file=CloudundancyDevOpsPythonTests/RunAllWithCoverage.py
-
-Set-PSDebug -Trace 0
+python.exe -u CloudundancyDevOpsPython\MypyFlake8PylintThenRunTestsWithCoverage.py --run-tests-with-coverage-python-file=CloudundancyDevOpsPythonTests/RunAllWithCoverage.py
 exit $LastExitCode
