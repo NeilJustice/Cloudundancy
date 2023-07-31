3257d7a42a40ae15d646eb4eaf077d544c0e73c6
Neil Justice <njjustice@gmail.com>
2023-07-30 21:56:55 -0500

    Remove JenkinsJobs/Windows/Cloudundancy-Release.ps1

--- JenkinsJobs/Windows/Cloudundancy-Release.ps1
+++
@@ -1,9 +0,0 @@
-$env:PYTHONPATH="."
-python.exe -u CloudundancyDevOpsPython\BuildAndInstallCPlusPlusProgram.py `
-   --solution-name=Cloudundancy `
-   --cmake-generator="Visual Studio 17 2022" `
-   --cmake-build-type=Release `
-   --tests-project-name=libCloudundancyTests `
-   --cmake-definitions="" `
-   --no-install
-exit $LastExitCode
