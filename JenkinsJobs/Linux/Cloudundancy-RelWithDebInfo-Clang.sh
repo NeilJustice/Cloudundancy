cd468d91811570aa009e3aa8c612ccd16005c936
Neil Justice <njjustice@gmail.com>
2025-01-11 17:27:17 -0500

    Fix JenkinsJobs/Linux/Cloudundancy-RelWithDebInfo-Clang.sh

--- JenkinsJobs/Linux/Cloudundancy-RelWithDebInfo-Clang.sh
+++ JenkinsJobs/Linux/Cloudundancy-RelWithDebInfo-Clang.sh
@@ -6,5 +6,6 @@ export CXX=clang++
 python -u CloudundancyDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=Cloudundancy \
    --cmake-build-type=RelWithDebInfo \
+   --cmake-definitions="-DFastLinuxReleaseBuildMode=ON" \
    --tests-project-name=libCloudundancyTests \
    --install
