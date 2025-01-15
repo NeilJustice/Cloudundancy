81ba2b9c42474616cff030cd735739d9b88603a5
Neil Justice <njjustice@gmail.com>
2025-01-14 19:07:07 -0500

    Update JenkinsJobs/Linux/Cloudundancy-RelWithDebInfo-ClangUndefinedBehaviorSanitizer.sh

--- JenkinsJobs/Linux/Cloudundancy-RelWithDebInfo-ClangUndefinedBehaviorSanitizer.sh
+++ JenkinsJobs/Linux/Cloudundancy-RelWithDebInfo-ClangUndefinedBehaviorSanitizer.sh
@@ -3,10 +3,9 @@ set -ev
 
 export PYTHONPATH=.
 export CXX=clang++
-export UBSAN_OPTIONS=halt_on_error=true
 python -u CloudundancyDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=Cloudundancy \
    --cmake-build-type=RelWithDebInfo \
    --tests-project-name=libCloudundancyTests \
-   --cmake-definitions="-DClangUndefinedBehaviorSanitizerMode=ON -DFastLinuxRelWithDebInfoBuildMode=ON" \
+   --cmake-definitions="-DClangUndefinedBehaviorSanitizerMode=ON" \
    --no-install
