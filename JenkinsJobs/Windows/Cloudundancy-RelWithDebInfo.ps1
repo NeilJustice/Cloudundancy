9d2cc54366274c85103dfe87adaad26e5c78c981
Neil Justice <njjustice@gmail.com>
2025-01-23 07:35:41 -0600

    JenkinsJobs/Windows/Cloudundancy-RelWithDebInfo.ps1: Update CMAKE_INSTALL_PREFIX from X:\ to C:\

--- JenkinsJobs/Windows/Cloudundancy-RelWithDebInfo.ps1
+++ JenkinsJobs/Windows/Cloudundancy-RelWithDebInfo.ps1
@@ -3,6 +3,6 @@ python.exe -u CloudundancyDevOpsPython\BuildAndInstallCPlusPlusProgram.py `
    --solution-name=Cloudundancy `
    --cmake-build-type=RelWithDebInfo `
    --tests-project-name=libCloudundancyTests `
-   --cmake-definitions="-DCMAKE_INSTALL_PREFIX=X:\\" `
+   --cmake-definitions="-DCMAKE_INSTALL_PREFIX=C:\\" `
    --install
 exit $LastExitCode
