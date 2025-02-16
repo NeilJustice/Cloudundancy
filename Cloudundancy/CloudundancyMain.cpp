f6f5538611611ef9fb19c593a1fcb2f169b84a47
Neil Justice <njjustice@gmail.com>
2025-02-16 12:26:23 -0600

    Add folders TestingIniFiles and TestingSourceFiles, and update example args in CloudundancyMain.cpp

--- Cloudundancy/CloudundancyMain.cpp
+++ Cloudundancy/CloudundancyMain.cpp
@@ -14,15 +14,18 @@ using namespace std;
 
 #include "libCloudundancy/Components/CloudundancyProgram.h"
 
-// Production Cloudundancy command lines:
-// copy-files-to-multiple-folders --ini-file=C:\Code\dotfiles\WindowsCloudundancy.ini --delete-destination-folders-first
-// 7zip-files-then-copy-the-7zip-file-to-multiple-folders --ini-file-to-copy-files-to-7zip-staging-folder=C:\Code\dotfiles\WindowsCloudundancy_CopyCodeFolderTo7ZipStagingFolder.ini --7zip-staging-folder=D:\CodeBackup --ini-file-to-copy-7zip-file-from-staging-folder-to-multiple-folders=C:\Code\dotfiles\WindowsCloudundancy_Copy7ZipFileFromStagingFolderToMultipleFolders.ini
-
-// Testing Cloudundancy command lines:
-// copy-files-to-multiple-folders --ini-file=D:\CloudundancyTesting\Cloudundancy.ini --delete-destination-folders-first
+// Testing Args
+// ============
+// copy-files-to-multiple-folders --ini-file=C:\Code\Cloudundancy\TestingIniFiles\WindowsCloudundancy.ini
 // 7zip-files-then-copy-the-7zip-file-to-multiple-folders --ini-file-to-copy-files-to-7zip-staging-folder=D:\Cloudundancy7ZipTesting\CodeFolderTo7ZipStagingFolderCopyingStep.ini --7zip-staging-folder=D:\Cloudundancy7ZipTesting\7ZipStagingFolder --ini-file-to-copy-7zip-file-from-staging-folder-to-multiple-folders=D:\Cloudundancy7ZipTesting\7ZipFileToGoogleDriveAndOneDriveCopyingStep.ini
 // example-linux-ini-file
 // example-windows-ini-file
+
+// Production Args
+// ===============
+// copy-files-to-multiple-folders --ini-file=C:\Code\PowerShellFiles\WindowsCloudundancy.ini
+// copy-files-to-multiple-folders --ini-file=C:\Code\PowerShellFiles\WindowsCloudundancy.ini --delete-destination-folders-first
+// 7zip-files-then-copy-the-7zip-file-to-multiple-folders --ini-file-to-copy-files-to-7zip-staging-folder=C:\Code\PowerShellFiles\WindowsCloudundancy_CopyCodeFolderTo7ZipStagingFolder.ini --7zip-staging-folder=D:\CodeBackup --ini-file-to-copy-7zip-file-from-staging-folder-to-multiple-folders=C:\Code\PowerShellFiles\WindowsCloudundancy_Copy7ZipFileFromStagingFolderToMultipleFolders.ini
 
 int main(int argc, char* argv[])
 {
