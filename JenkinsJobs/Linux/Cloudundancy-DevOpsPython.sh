900dfca4dac194afbe6f2644f31acb3adf4c3954
Neil Justice <njjustice@gmail.com>
2025-06-15 17:24:45 -0500

    Fix JenkinsJobs/Linux/Cloudundancy-DevOpsPython.sh

--- JenkinsJobs/Linux/Cloudundancy-DevOpsPython.sh
+++ JenkinsJobs/Linux/Cloudundancy-DevOpsPython.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -ev
 
+cd CloudundancyDevOpsPython
 export PYTHONPATH=.
 python CloudundancyDevOpsPython/MypyFlake8PylintThenRunTestsWithCoverage.py --run-tests-with-coverage-python-file=CloudundancyDevOpsPythonTests/RunAllWithCoverage.py
+cd ..
