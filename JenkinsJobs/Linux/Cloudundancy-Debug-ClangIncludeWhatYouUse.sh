a091eedd9b438c17bad41b63961c2be13c5f67cc
Neil Justice <njjustice@gmail.com>
2025-07-07 18:15:34 -0500

    Fix JenkinsJobs/Linux/Cloudundancy-Debug-ClangIncludeWhatYouUse.sh

--- JenkinsJobs/Linux/Cloudundancy-Debug-ClangIncludeWhatYouUse.sh
+++ JenkinsJobs/Linux/Cloudundancy-Debug-ClangIncludeWhatYouUse.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/bash
 set -ev
 
+export PYTHONPATH=CloudundancyDevOpsPython
 export CXX=clang++
-python -u /usr/local/bin/PyUtils/BuildAndInstallCPlusPlusStaticLibrary.py \
+python -u CloudundancyDevOpsPython/CloudundancyDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=Cloudundancy \
    --cmake-build-type=Debug \
    --cmake-definitions="-DIncludeWhatYouUseMode=ON" \
-   --tests-project-name=CloudundancyTests \
-   --do-install=false
-
+   --tests-project-name=libCloudundancyTests \
+   --no-install
