44a7ba97a930f8116f60f080655d6f6575c0fd77
Neil Justice <njjustice@gmail.com>
2025-12-07 10:53:52 -0600

    Implement JenkinsJobs/Windows/Cloudundancy-Debug-ReadLLVMCodeCoverageReport.ps1

---
+++ JenkinsJobs/Windows/Cloudundancy-Debug-ReadLLVMCodeCoverageReport.ps1
@@ -0,0 +1,2 @@
+LinuxCodeCoverageRunner.exe read-llvm-code-coverage-text-report `
+   --llvm-code-coverage-text-report="C:\CodeCoverageReports\Cloudundancy\covhtml\Cloudundancy_llvm_code_coverage_report.txt"
