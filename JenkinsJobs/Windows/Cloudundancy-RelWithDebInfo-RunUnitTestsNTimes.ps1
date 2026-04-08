d11495950772b59a9cf2e4e053666a4f70c13693
Neil Justice <njjustice@gmail.com>
2026-04-08 07:34:13 -0500

    Add JenkinsJobs/Windows/Cloudundancy-RelWithDebInfo-RunUnitTestsNTimes.ps1

---
+++ JenkinsJobs/Windows/Cloudundancy-RelWithDebInfo-RunUnitTestsNTimes.ps1
@@ -0,0 +1,4 @@
+WindowsCPlusPlusBuilder.exe build-cpp-solution-then-run-unit-tests-n-times `
+   --solution-name=Cloudundancy `
+   --tests-project-name=libCloudundancyTests `
+   --configuration=RelWithDebInfo
