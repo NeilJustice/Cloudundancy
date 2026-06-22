69021780a88e06eab22a0e898d5fbeb37d151f2c
Neil Justice <njjustice@gmail.com>
2026-06-21 22:56:34 -0500

    Comment out JenkinsJobs/Linux/Cloudundancy-Debug-Clang-IncludeWhatYouUse.sh

--- JenkinsJobs/Linux/Cloudundancy-Debug-Clang-IncludeWhatYouUse.sh
+++ JenkinsJobs/Linux/Cloudundancy-Debug-Clang-IncludeWhatYouUse.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/bash
-set -ev
+#set -ev
 
-export PYTHONPATH=CloudundancyDevOpsPython
-export CXX=clang++
-python -u CloudundancyDevOpsPython/CloudundancyDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
-   --solution-name=Cloudundancy \
-   --cmake-build-type=Debug \
-   --cmake-definitions="-DIncludeWhatYouUseMode=ON" \
-   --tests-project-name=libCloudundancyTests \
-   --no-install
+#export PYTHONPATH=CloudundancyDevOpsPython
+#export CXX=clang++
+#python -u CloudundancyDevOpsPython/CloudundancyDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
+#   --solution-name=Cloudundancy \
+#   --cmake-build-type=Debug \
+#   --cmake-definitions="-DIncludeWhatYouUseMode=ON" \
+#   --tests-project-name=libCloudundancyTests \
+#   --no-install
+
