1b38db0b203106e96519c11535640be6bc992e83
Neil Justice <njjustice@gmail.com>
2026-06-28 11:13:06 -0500

    Uncomment contents of Cloudundancy-Debug-Clang-IncludeWhatYouUse.sh

--- JenkinsJobs/Linux/Cloudundancy-Debug-Clang-IncludeWhatYouUse.sh
+++ JenkinsJobs/Linux/Cloudundancy-Debug-Clang-IncludeWhatYouUse.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/bash
-#set -ev
+set -ev
 
-#export PYTHONPATH=CloudundancyDevOpsPython
-#export CXX=clang++
-#python -u CloudundancyDevOpsPython/CloudundancyDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
-#   --solution-name=Cloudundancy \
-#   --cmake-build-type=Debug \
-#   --cmake-definitions="-DIncludeWhatYouUseMode=ON" \
-#   --tests-project-name=libCloudundancyTests \
-#   --no-install
-
+export CXX=clang++
+LinuxCPlusPlusBuilder build-cpp-program \
+   --solution-name=Cloudundancy \
+   --configuration=Debug \
+   --cmake-definitions="-DIncludeWhatYouUseMode=ON" \
+   --install=false \
+   --standard-output-file="/code/IncludeWhatYouUseReports/Cloudundancy_iwyu_report.txt"
