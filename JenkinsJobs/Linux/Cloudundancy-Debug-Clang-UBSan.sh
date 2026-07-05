43ccccce1a2abefa1310e5df63276da4aeec586e
Neil Justice <njjustice@gmail.com>
2026-07-05 18:11:35 -0500

    Add UBSAN_OPTIONS='silence_unsigned_overflow=1' to JenkinsJobs/Linux/Cloudundancy-Debug-Clang-UBSan.sh

--- JenkinsJobs/Linux/Cloudundancy-Debug-Clang-UBSan.sh
+++ JenkinsJobs/Linux/Cloudundancy-Debug-Clang-UBSan.sh
@@ -3,6 +3,7 @@ set -ev
 
 export PYTHONPATH=CloudundancyDevOpsPython
 export CXX=clang++
+export UBSAN_OPTIONS="silence_unsigned_overflow=1"
 python -u CloudundancyDevOpsPython/CloudundancyDevOpsPython/BuildAndInstallCPlusPlusProgram.py \
    --solution-name=Cloudundancy \
    --cmake-build-type=Debug \
