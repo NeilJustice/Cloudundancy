bdb3097c2a615cd239cfe88e191eab0fc092cda6
Neil Justice <njjustice@gmail.com>
2026-08-30 23:00:18 -0500

    Update JenkinsJobs/Windows/Cloudundancy-Debug.ps1 and JenkinsJobs/Windows/Cloudundancy-RelWithDebInfo.ps1

--- JenkinsJobs/Windows/Cloudundancy-Debug.ps1
+++ JenkinsJobs/Windows/Cloudundancy-Debug.ps1
@@ -1,5 +1,6 @@
 WindowsCPlusPlusBuilder.exe build-cpp-solution `
    --solution-name=Cloudundancy `
    --configuration=Debug `
+   --cmake-definitions="" `
    --install=false
 exit $LastExitCode
--- JenkinsJobs/Windows/Cloudundancy-RelWithDebInfo.ps1
+++ JenkinsJobs/Windows/Cloudundancy-RelWithDebInfo.ps1
@@ -1,5 +1,6 @@
 WindowsCPlusPlusBuilder.exe build-cpp-solution `
    --solution-name=Cloudundancy `
    --configuration=RelWithDebInfo `
+   --cmake-definitions="" `
    --install=true
 exit $LastExitCode
